3fc724f8a2c4fe8ef2af07f2853a1a60abf0aa31
Qarib2004 <garibalisultanov@gmail.com>
2026-04-24 20:51:02 +0400

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
       '@shared': '/src/shared'
     }
   },
+  define: {
+    'import.meta.env.VITE_SERVER_URL': JSON.stringify(process.env.VITE_SERVER_URL)
+  },
   server: {
     port: 3001,
     proxy: {
